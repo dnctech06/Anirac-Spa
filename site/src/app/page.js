9aaf6b6acbc1f60dbad8899775e423baee466d9f
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-03-02 23:18:24 -0300

    implementação cor gradiente css

--- site/src/app/page.js
+++ site/src/app/page.js
@@ -3,13 +3,18 @@ import Navbar from '@/components/a_navbar';
 import Header from '@/components/b_header';
 import MainHome from '@/components/d_main_home';
 import Footer from '@/components/c_footer';
-import style from './globals.css';
+import style from './style.css';
 
 
 
 export default function Home() {
   return (
     <main >
+      <div className='um' >ola mundo</div>
+      <div className='dois' >ola mundo</div>
+      <div className='tres' >ola mundo</div>
+
+      
       <Navbar></Navbar>
       <Header></Header>
       <MainHome></MainHome>

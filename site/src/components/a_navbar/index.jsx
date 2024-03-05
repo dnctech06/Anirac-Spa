5d5f40161c4f46452319c11e5e6ed5e6a5de4a60
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-03-04 22:13:21 -0300

    finalização navbar Matheus

--- site/src/components/a_navbar/index.jsx
+++ site/src/components/a_navbar/index.jsx
@@ -1,11 +1,27 @@
 import Image from 'next/image';
 import style from './style.css';
-import Foto1 from '../../assets/img/espaco/1.jpg';
+import Logo from '../../assets/img/servico/logo.jpg';
+import Face from '../../assets/img/servico/facebookVerde.png';
+import Insta from '../../assets/img/servico/instagraVerdem.png';
+import Link from 'next/link';
+
 
 const Navbar = () => {
-    return(
-        <div>
-            <Image src={Foto1} alt="Foto render" />
+    return (
+        <div className='Full_navbar'>
+           <a href="/"><Image   className='logo' src={Logo} alt="Foto logo" /> </a> 
+            <div className='navbar_btn'>
+                <Link id='Home' className='lks' href="/">Início</Link>
+                <Link id='Home' className='lks' href="sobreNos">Sobre Nós</Link>
+                <Link id='Home' className='lks' href="cuidados">Cuidados</Link>
+                <Link id='Home' className='lks' href="contatos">Contatos</Link>
+            </div>
+            <div className='navbar_links'>
+                <a target="_blank" href="https://www.instagram.com/aniracspa/?igsh=aWthaDhzN3g4ZDNz"><Image className='lks_out' src={Insta} alt="Foto logo" /></a>
+                <a target="_blank" href="https://m.facebook.com/profile.php/?id=100083463273920"><Image className='lks_out' src={Face} alt="Foto logo" /></a>
+
+
+            </div>
         </div>
     )
 }

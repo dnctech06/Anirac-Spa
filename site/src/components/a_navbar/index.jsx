3266e8e4189fff1ce400b9562543a5fc3aff051c
Eduardo-lima92 <dudulima2005@hotmail.com>
2024-03-05 22:08:54 -0300

    Criação da sessão quem somos e mapa/endereços.
    Criação da sessão quem somos e mapa/endereços. Realizado inserção dos ícones das redes sociais.

--- site/src/components/a_navbar/index.jsx
+++ site/src/components/a_navbar/index.jsx
@@ -1,12 +1,10 @@
-import Image from 'next/image';
 import style from './style.css';
-import Foto1 from '../../assets/img/espaco/1.jpg';
 
 const Navbar = () => {
     return(
-        <div>
-            <Image src={Foto1} alt="Foto render" />
-        </div>
+        <main>
+            Aqui vai o navbar, responsável Matheus Nascimento
+        </main>
     )
 }
 
--- site/src/components/d_main_home/index.jsx
+++ site/src/components/d_main_home/index.jsx
@@ -1,9 +1,44 @@
+import Image from 'next/image';
 import style from './style.css';
+import Foto1 from '../../assets/img/espaco/4.png';
+import Foto2 from '../../assets/img/espaco/10.png';
+import Foto3 from '../../assets/img/espaco/37.jpg';
+<style>
+import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap')
+</style>
 
 const MainHome = () => {
     return(
         <main>
-            Aqui vai o quem somos resumido, responsável Eduardo.
+            <div className='whoWeAre'>
+                <div>
+                    <h3>Quem somos?</h3>
+                    <p>Corpo mente e alma, este é o nosso lema.<br/> 
+                    Tratamos o individuo como um todo.<br/><br/>
+                    O nosso espaço oferece diversos serviços,<br/>
+                    massagens terapêuticas, massagens bem estar,<br/>
+                    massagens orientais, osteopatia, osteopatia pediátrica,<br/>
+                    mesoterapia, tratamentos estéticos, estética avançada,<br/>
+                    drenagem linfática, hidrolinfa, lifting pestanas,<br/>
+                    tratamentos de rosto, limpeza de pele profunda,<br/>
+                    pedicure, manicure, gel e verniz de gel, reflexologia,<br/>
+                    Reik com toque terapêutico.</p>
+
+                </div>
+
+                <div className='imageswho'>
+                    <div>
+                        <Image id='foto1' src={Foto1} alt="Foto render" height={360} width={360} />
+                        <Image id='foto2' src={Foto2} alt="Foto render" height={230} width={250}/>
+                        <Image id='foto3' src={Foto3} alt="Foto render" height={400} width={300}/>
+                    </div>
+                                    
+
+                </div>
+
+
+            </div>
+            
         </main>
     )
 }

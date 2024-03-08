576d1f109f9b21130d8fb228ed0de32ba6e0d545
Brandolt55 <gabrielbrandolt55@gmail.com>
2024-03-08 12:36:52 -0300

    criando a função do carrocel e estilização, e a instalação da biblioteca slick-carousel

--- site/src/components/b_header/index.jsx
+++ site/src/components/b_header/index.jsx
@@ -1,10 +1,28 @@
 import style from './style.css';
+import Carrocel from './Carrocel'
+  
+  
 
 const Header = () => {
     return(
         <main className='carrocel'>
-            <div>
-                
+            <div className='conteudo'>
+                <Carrocel/>
+            </div>
+            <div className='conteudo'>
+                <h4>
+                Lorem ipsum dolor sit amet, consectetu<br/>
+                <br/>
+                erat, vel vestibulum diam. Curabitur sed<br/>
+                arcu sodales massa gravida rhoncus. N<br/>
+                <br/>
+                it. Nunc efficitur sapien quam, quis hendr<br/>
+                erit justo egestas sed. Quisque mollis feli<br/>
+                <br/>
+                <br/>
+                Quisque faucibus erat ac mi interdum gravida.<br/>
+                Nunc blandit ante non lorem aliquam consequat.<br/>
+                </h4>
             </div>
         </main>
     )

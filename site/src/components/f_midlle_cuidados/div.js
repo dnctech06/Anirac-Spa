5f8de03445d17a449240f9c96af7d74d16afc169
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-03-07 23:42:27 -0300

    estilização página cuidados

--- site/src/components/f_midlle_cuidados/div.js
+++ site/src/components/f_midlle_cuidados/div.js
@@ -2,26 +2,40 @@ import { useState } from "react";
 
 import style from "./style.css";
 import Massagem from "../../assets/img/espaco/linda-loira-recebendo-uma-massagem.jpg";
-const v40 = "- 40,00€";
-const v45 = "- 45,00€";
-const v60 = "- 60,00€";
-const v150 = "- 150,00€";
-const v27 = "- 27,00€";
-const v25 = "- 25,00€";
-const v20 = "- 20,00€";
-const v18 = "- 18,00€";
-const v80 = "- 80,00€";
-const v99 = "- 99,00€";
-const v100 = "- 100,00€";
-const v200 = "- 200,00€";
-const v275 = "- 275,00€";
-const v250 = "- 250,00€";
+
+const v15 = " - 15,00€ / fio";
+
+const v40 = " - 40,00€";
+const v45 = " - 45,00€";
+const v60 = " - 60,00€";
+const v150 = " - 150,00€";
+const v27 = " - 27,00€";
+const v25 = " - 25,00€";
+const v20 = " - 20,00€";
+const v18 = " - 18,00€";
+const v75 = " - 75,00€ / fio";
+
+const v80 = " - 80,00€";
+const v99 = " - 99,00€";
+const v100 = " - 100,00€";
+const v200 = " - 200,00€";
+const v275 = " - 275,00€";
+const v250 = " - 250,00€";
+const v400 = " - 400,00€";
+const v450 = " - 450,00€";
 
 function Div() {
   const [info, setInfo] = useState(false);
 
   function InfoOn() {
-    setInfo(true);
+    if(info === true){
+      setInfo(false);
+    }else{
+      setInfo(true);
+
+    }
+
+    
   }
 
   function InfoOff() {
@@ -29,10 +43,11 @@ function Div() {
   }
 
   return (
-    <div className="Full_service" onMouseLeave={InfoOff} onMouseEnter={InfoOn}>
+    <div className="Full_service" onClick={InfoOn}>
       <div className="Title_FullBody">
-        <h1>CUIDADOS</h1>
-        <h1>CORPO </h1>
+        <h1>Cuidados</h1>
+        <h1>Corpo </h1>
+        <h1>Completo </h1>
         <h1></h1>
       </div>
 
@@ -41,48 +56,48 @@ function Div() {
       {info === true && (
         <section className="valores">
           <div className="discription">
-            <p>Ácido Marcas Acne</p> <p>{v40}</p>
+            <p>Ácido Marcas Acne</p> <pre> </pre> <p> {v40}</p>
           </div>
           <div className="discription">
-            <p>Ácido Pele Sensível</p> <p>{v40}</p>
+            <p>Ácido Pele Sensível</p> <pre> </pre> <p>{v40}</p>
           </div>
           <div className="discription">
-            <p>Ácidos Manchas</p> <p>{v40}</p>
+            <p>Ácidos Manchas</p><pre> </pre>  <p>{v40}</p>
           </div>
           <div className="discription">
-            <p>Ácido Redução Rugas</p> <p>{v40}</p>
+            <p>Ácido Redução Rugas</p> <pre> </pre> <p>{v40}</p>
           </div>
           <div className="discription">
-            <p>Fibroína de Seda</p> <p>{v60}</p>
+            <p>Fibroína de Seda</p> <pre> </pre> <p>{v60}</p>
           </div>
           <h2>Packs</h2>
 
           <div className="discription">
-            <p>Fibroína de Seda 3 sessões</p> <p>{v150}</p>
+            <p>Fibroína de Seda 3 sessões</p><pre> </pre>  <p>{v150}</p>
           </div>
           <div className="discription">
-            <p>Manutenção com Mãos de Seda</p> <p>{v27}</p>
+            <p>Manutenção com Mãos de Seda</p> <pre> </pre> <p>{v27}</p>
           </div>
           <div className="discription">
-            <p>Verniz de gel com Mãos de Seda</p> <p>{v20}</p>
+            <p>Verniz de gel com Mãos de Seda</p> <pre> </pre> <p>{v20}</p>
           </div>
           <div className="discription">
-            <p>Pedicure completa com Verniz de gel</p> <p>{v25}</p>
+            <p>Pedicure completa com Verniz de gel</p><pre> </pre>  <p>{v25}</p>
           </div>
           <div className="discription">
-            <p>Pedicure normal com Verniz de gel</p> <p>{v18}</p>
+            <p>Pedicure normal com Verniz de gel</p><pre> </pre>  <p>{v18}</p>
           </div>
           <div className="discription">
-            <p>Hidrolinfa Pack de 10</p> <p>{v100} Válidade 3 meses</p>
+            <p>Hidrolinfa Pack de 10</p><pre> </pre>  <p>{v100} Válidade 3 meses</p>
           </div>
           <div className="discription">
-            <p>Terapêutica zona 40' Pack de 3</p> <p>{v80}</p>
+            <p>Terapêutica zona 40' Pack de 3</p> <pre> </pre> <p>{v80}</p>
           </div>
           <div className="discription">
-            <p>Massagem de relaxamento Pack de 3</p> <p>{v99}</p>
+            <p>Massagem de relaxamento Pack de 3</p> <pre> </pre> <p>{v99}</p>
           </div>
           <div className="discription">
-            <p>Barra de Acess Pack de 5</p> <p>{v275}</p>
+            <p>Barra de Acess Pack de 5</p> <pre> </pre> <p>{v275}</p>
           </div>
         </section>
       )}
@@ -90,52 +105,48 @@ function Div() {
       {info === true && (
         <section className="valores">
           <div className="discription">
-            <p>Osteopatia</p> <p>{v45}</p>
+            <p>Osteopatia</p> <pre> </pre> <p>{v45}</p>
           </div>
           <div className="discription">
-            <p>Osteopatia Pediátrica</p> <p>{v45}</p>
+            <p>Osteopatia Pediátrica</p> <pre> </pre> <p>{v45}</p>
           </div>
           <div className="discription">
-            <p>Massoterapia</p> <p>{v45}</p>
+            <p>Massoterapia</p><pre> </pre>  <p>{v45}</p>
           </div>
           <div className="discription">
-            <p>Terapia Sacrocraniana</p> <p>{v40}</p>
+            <p>Terapia Sacrocraniana</p> <pre> </pre> <p>{v40}</p>
           </div>
           <h2>Harmonização Facial</h2>
 
           <div className="discription">
-            <p>Full Face (4 zonas)</p> <p>{v250}</p>
+            <p>Full Face (4 zonas)</p> <pre> </pre> <p>{v250}</p>
           </div>
 
           <div className="discription">
-            <p>Full Face (1 zona)</p> <p>{v150}</p>
+            <p>Full Face (1 zona)</p> <pre> </pre> <p>{v150}</p>
           </div>
           <h2>Ácido Hilurónico</h2>
 
           <div className="discription">
-            <p>1 Seringa 1ml</p> <p>{v200}</p>
+            <p>1 Seringa 1ml</p> <pre> </pre> <p>{v200}</p>
+          </div>
+          <h2>Boestimuladores</h2>
+
+          <div className="discription">
+            <p>Radiesse</p> <pre> </pre> <p>{v400}</p>
           </div>
           <div className="discription">
-            <p>Verniz de gel com Mãos de Seda</p> <p>{v20}</p>
+            <p>Sculptra / Elleva</p> <pre> </pre> <p>{v450}</p>
+          </div>
+          <h2>Fios de PDO</h2>
+          <div className="discription">
+            <p>Multi / Spring</p> <pre> </pre> <p>{v15}</p>
           </div>
           <div className="discription">
-            <p>Pedicure completa com Verniz de gel</p> <p>{v25}</p>
+            <p>Multi-fill</p> <pre> </pre> <p>{v75}</p>
           </div>
-          <div className="discription">
-            <p>Pedicure normal com Verniz de gel</p> <p>{v18}</p>
-          </div>
-          <div className="discription">
-            <p>Hidrolinfa Pack de 10</p> <p>{v100} Válidade 3 meses</p>
-          </div>
-          <div className="discription">
-            <p>Terapêutica zona 40' Pack de 3</p> <p>{v80}</p>
-          </div>
-          <div className="discription">
-            <p>Massagem de relaxamento Pack de 3</p> <p>{v99}</p>
-          </div>
-          <div className="discription">
-            <p>Barra de Acess Pack de 5</p> <p>{v275}</p>
-          </div>
+          
+          
         </section>
       )}
     </div>
--- site/src/components/f_midlle_cuidados/index.jsx
+++ site/src/components/f_midlle_cuidados/index.jsx
@@ -1,12 +1,14 @@
 
 import style from './style.css';
 import Div from './div';
+import Div2 from './div2';
 
 
 const MidllleCuidados = () => {
     return(
         <main className='Full_MidllleCuidados'>
             <Div></Div>
+            <Div2></Div2>
             
         </main>
     )

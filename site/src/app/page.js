ed39158295ffff630c852931ce8a83c10675bbc8
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-03-12 09:36:29 -0300

    mescragem branch gabriel alisson e ajustes

--- site/src/app/page.js
+++ site/src/app/page.js
@@ -4,13 +4,14 @@ import Header from '@/components/b_header';
 import MainHome from '@/components/d_main_home';
 import Footer from '@/components/c_footer';
 import style from './style.css';
-
+import Carrossel from '@/components/b_header_carrossel/Carrocel';
 
 
 export default function Home() {
   return (
     <main >      
       <Navbar></Navbar>
+      <Carrossel></Carrossel>
       <Header></Header>
       <MainHome></MainHome>
       <Footer></Footer>
---
+++ site/src/components/b_header_carrossel/Carrocel.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import style from './carrocel.css';
+
+function Carrossel() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 700,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000
+      
+    };
+  
+    return (
+      <Slider {...settings} className='estilo'>
+        <div>
+          <img src='next.svg' alt="Imagem 1" />
+        </div>
+        <div>
+          <img src='vercel.svg'alt="Imagem 2" />
+        </div>
+        <div>
+          <img src='next.svg' alt="Imagem 3" />
+        </div>
+        <div>
+          <img src='vercel.svg' alt="Imagem 4" />
+        </div>
+        <div>
+          <img src='next.svg' alt="Imagem 5" />
+        </div>
+        {/* Adicione mais imagens conforme necessário */}
+      </Slider>
+    );
+  }
+
+export default Carrossel;

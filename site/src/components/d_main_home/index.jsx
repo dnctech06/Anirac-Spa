8c1fc546c9f94d0b3e23ba94fcaae74e4b9f5c26
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-03-17 17:35:09 -0300

    instalação notflix page cuidado

--- site/src/components/d_main_home/index.jsx
+++ site/src/components/d_main_home/index.jsx
@@ -2,40 +2,35 @@ import Image from 'next/image';
 import style from './style.css';
 import Foto1 from '../../assets/img/espaco/colagem.png';
 <style>
-import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap')
+    import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap')
 </style>
 
 const MainHome = () => {
-    return(
+    return (
         <main>
             <div className='whoWeAre'>
                 <div className='txt'>
                     <h3>Quem somos?</h3>
                     <p>Corpo mente e alma, este é o nosso lema.
-                    Tratamos o individuo como um todo.
-                    O nosso espaço oferece diversos serviços,
-                    massagens terapêuticas, massagens bem estar,
-                    massagens orientais, osteopatia, osteopatia pediátrica,
-                    mesoterapia, tratamentos estéticos, estética avançada,
-                    drenagem linfática, hidrolinfa, lifting pestanas,
-                    tratamentos de rosto, limpeza de pele profunda,
-                    pedicure, manicure, gel e verniz de gel, reflexologia,
-                    Reik com toque terapêutico.</p>
+                        Tratamos o individuo como um todo.
+                        O nosso espaço oferece diversos serviços,
+                        massagens terapêuticas, massagens bem estar,
+                        massagens orientais, osteopatia, osteopatia pediátrica,
+                        mesoterapia, tratamentos estéticos, estética avançada,
+                        drenagem linfática, hidrolinfa, lifting pestanas,
+                        tratamentos de rosto, limpeza de pele profunda,
+                        pedicure, manicure, gel e verniz de gel, reflexologia,
+                        Reik com toque terapêutico.</p>
 
                 </div>
 
                 <div className='imageswho'>
                     <div>
                         <Image id='foto1' className='ImgWho' src={Foto1} alt="Foto render" />
-                        
+
                     </div>
-                                    
-
                 </div>
-
-
             </div>
-            
         </main>
     )
 }
--- site/src/components/f_midlle_cuidados/index.jsx
+++ site/src/components/f_midlle_cuidados/index.jsx
@@ -1,21 +1,35 @@
-
 import style from './style.css';
 import Div from './div';
 import Div2 from './div2';
 import Div3 from './div3';
 import Div4 from './div4';
-
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { useEffect } from 'react';
 
 const MidllleCuidados = () => {
+    // Adicionar indicador de carregamento ao carregar a página
+    useEffect(() => {
+        // Exibir indicador de carregamento
+        Loading.dots('Aguarde...');
+
+        // Remover indicador de carregamento após 4 segundos
+        const timer = setTimeout(() => {
+            Loading.remove();
+        }, 3500);
+
+        return () => {
+            clearTimeout(timer); // Limpar o temporizador quando o componente for desmontado
+        };
+    }, []);
+
     return(
         <main className='Full_MidllleCuidados'>
             <Div></Div>
             <Div2></Div2>
             <Div3></Div3>
             <Div4></Div4>
-            
         </main>
-    )
+    );
 }
 
 export default MidllleCuidados;
